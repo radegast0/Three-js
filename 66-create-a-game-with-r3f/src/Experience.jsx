81f85b8e665888e16ee74bd54ff24c47982ba3bf
radegast0 <fatih.yonucuoglu@gmail.com>
2024-07-02 10:57:20 +0300

    02.07

--- 66-create-a-game-with-r3f/src/Experience.jsx
+++ 66-create-a-game-with-r3f/src/Experience.jsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from '@react-three/drei';
 import Lights from './Lights.jsx';
 import Level from './Level.jsx';
 import { Physics } from '@react-three/rapier';
@@ -7,7 +6,6 @@ import { Player } from './Player.jsx';
 export default function Experience() {
 	return (
 		<>
-			<OrbitControls makeDefault />
 			<Physics debug={false}>
 				<Lights />
 				<Level />
--- 66-create-a-game-with-r3f/src/Player.jsx
+++ 66-create-a-game-with-r3f/src/Player.jsx
@@ -1,12 +1,15 @@
 import { useKeyboardControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { RigidBody, useRapier } from '@react-three/rapier';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
 
 export const Player = () => {
 	const [subscribeKeys, getKeys] = useKeyboardControls();
 	const body = useRef();
 	const { rapier, world } = useRapier();
+	const [smoothedCameraPosition] = useState(() => new THREE.Vector3(10,10,10));
+	const [smoothedCameraTarget] = useState(() => new THREE.Vector3());
 
 	const jump = () => {
 		const origin = body.current.translation();
@@ -35,6 +38,9 @@ export const Player = () => {
 		};
 	}, []);
 	useFrame((state, delta) => {
+		/**
+		 * Controls
+		 */
 		const { forward, backward, left, right } = getKeys();
 		const impulse = { x: 0, y: 0, z: 0 };
 		const torque = { x: 0, y: 0, z: 0 };
@@ -61,6 +67,26 @@ export const Player = () => {
 
 		body.current.applyImpulse(impulse);
 		body.current.applyTorqueImpulse(torque);
+
+		/**
+		 * Camera
+		 */
+
+		const bodyPosition = body.current.translation();
+		const cameraPosition = new THREE.Vector3();
+		cameraPosition.copy(bodyPosition);
+		cameraPosition.z += 2.25;
+		cameraPosition.y += 0.65;
+
+		const cameraTarget = new THREE.Vector3();
+		cameraTarget.copy(bodyPosition);
+		cameraTarget.y += 0.25;
+
+		smoothedCameraPosition.lerp(cameraPosition, 5 * delta);
+		smoothedCameraTarget.lerp(cameraTarget, 5 * delta);
+
+		state.camera.position.copy(smoothedCameraPosition);
+		state.camera.lookAt(smoothedCameraTarget);
 	});
 	return (
 		<RigidBody
